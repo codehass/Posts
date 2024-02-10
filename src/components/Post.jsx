5672e006e0264e8d7080d7a26bfeaa5a983420cb
codehass <hassan.elouardy06@gmail.com>
2024-02-10 15:21:34 +0100

    add delete post functionality to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,19 +2,43 @@ import React from "react";
 import { countWords } from "./utils/countWords";
 import Link from "next/link";
 
-function Post({ postProp }) {
-	const { userId, id, title, body } = postProp;
+function Post({ postProp, onDelete }) {
+	const { id, userId, title, body } = postProp;
 	const wordCount = countWords(body);
 
+	const deleteHandler = async () => {
+		try {
+			const response = await fetch(
+				`https://jsonplaceholder.typicode.com/posts/${id}`,
+				{
+					method: "DELETE",
+				}
+			);
+
+			if (response.ok) {
+				onDelete(id);
+			} else {
+				console.error(`Failed to delete post with id ${id}`);
+			}
+		} catch (error) {
+			console.error("Error while deleting post:", error);
+		}
+	};
+
 	return (
-		<div className="m-2 border ">
-			<Link href={`/posts/${id}`}>
+		<div className="m-2 border flex justify-center items-center">
+			<Link href={`/posts/${id}`} className="w-[95%]">
 				<p>{userId} </p>
 				<p>{id} </p>
 				<p>{title} </p>
 				<p>{body} </p>
 				<p>{wordCount} </p>
 			</Link>
+			<div className="w-[5%]">
+				<button className="text-red-500" onClick={deleteHandler}>
+					Delete
+				</button>
+			</div>
 		</div>
 	);
 }
